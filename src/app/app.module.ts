0fb1c3f6e9afaece07375f27b6624e70932a9f39
Volodymyr Ilemskyi <vilemskyi@softserveinc.com>
2020-06-09 21:14:57 +0300

    weat-3: developed unit tests for menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
--- src/app/menu/desktop-menu/desktop-menu.component.spec.ts
+++ src/app/menu/desktop-menu/desktop-menu.component.spec.ts
@@ -1,25 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { configMenu } from '../config-menu';
 import { DesktopMenuComponent } from './desktop-menu.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('DesktopMenuComponent', () => {
   let component: DesktopMenuComponent;
   let fixture: ComponentFixture<DesktopMenuComponent>;
+  let element: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DesktopMenuComponent ]
-    })
-    .compileComponents();
+      declarations: [DesktopMenuComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DesktopMenuComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    element = fixture.debugElement.nativeElement;
+    fixture.detectChanges();    
   });
 
-  it('should create', () => {
+  it('should create DesktopMenuComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render menu items', () => {
+    const items = element.querySelectorAll('a');
+    expect(items.length).toBe(configMenu.length);
+  });
+
+  it("should render right menu item's hrefs", () => {
+    const items: NodeListOf<HTMLElement> = element.querySelectorAll('a');
+    items.forEach((item, i) => {
+      expect(item.getAttribute('href')).toBe(configMenu[i].path);
+    });
+  });
+
+  it("should render right menu item's labels", () => {
+    const items: NodeListOf<HTMLElement> = element.querySelectorAll('a');
+    items.forEach((item, i) => {
+      expect(item.textContent.trim()).toMatch(configMenu[i].label);
+    });
   });
 });
